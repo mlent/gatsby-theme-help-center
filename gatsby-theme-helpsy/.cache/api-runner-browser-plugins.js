d8b38c9e7703171beab58eafbb9fc28b19992e32
Monica Lent <lent.monica@gmail.com>
2019-11-01 21:07:59 +0100

    Remove ignored files.

--- gatsby-theme-helpsy/.cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../../node_modules/gatsby-plugin-material-ui/gatsby-browser.js'),
-      options: {"plugins":[],"stylesProvider":{"injectFirst":true}},
-    }]
--- gatsby-theme-helpsy/.cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-home-tsx": () => import("../src/home.tsx" /* webpackChunkName: "component---src-home-tsx" */),
-  "component---src-templates-category-tsx": () => import("../src/templates/category.tsx" /* webpackChunkName: "component---src-templates-category-tsx" */),
-  "component---cache-dev-404-page-js": () => import("dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
-}
-
