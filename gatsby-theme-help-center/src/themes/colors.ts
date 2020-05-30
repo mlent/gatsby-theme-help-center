698609ff38eb716402d80b4e13bd92c0aabe7242
Monica Lent <lent.monica@gmail.com>
2020-05-30 15:18:50 +0200

    Do not use "white" in material-ui theme

--- gatsby-theme-help-center/src/themes/colors.ts
+++ gatsby-theme-help-center/src/themes/colors.ts
@@ -3,30 +3,30 @@ export const COLORS = {
     light: '#63ccff',
     main: '#4285f4',
     dark: '#006db3',
-    contrastText: 'white',
+    contrastText: '#FFFFFF'
   },
   secondary: {
     light: '#ff8cb3',
     dark: '#c51162',
     main: '#f50057',
-    contrastText: 'white',
+    contrastText: '#FFFFFF'
   },
   success: {
     light: '#ADE488',
     dark: '#237804',
     main: '#52c41a',
-    contrastText: 'white',
+    contrastText: '#FFFFFF'
   },
   pending: {
     light: '#F5E18C',
     dark: '#F6CC1B',
     main: '#F6CC1B',
-    contrastText: 'white',
+    contrastText: '#FFFFFF'
   },
   error: {
     light: '#F7CACA',
     dark: '#DB4D4D',
     main: '#DB4D4D',
-    contrastText: 'white',
-  },
+    contrastText: '#FFFFFF'
+  }
 };
